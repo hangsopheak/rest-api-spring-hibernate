e7761e0eecb4535a8307c134c394cb948f5a29d4
sopheamak <sopheamak@gmail.com>
2017-02-22 19:26:01 +0700

    update sql

--- sql_test.sql
+++ sql_test.sql
@@ -1,3 +1,16 @@
+CREATE TABLE Person (
+ id int(10) unsigned NOT NULL AUTO_INCREMENT,
+ name VARCHAR(200), 
+ country VARCHAR(200),
+ version int(11),
+ createdDate timestamp,
+ updatedDate timestamp
+ 
+ );
+
+ INSERT INTO Person (id, name, country) values(1,'sophea', 'Cambodia');
+ 
+
 DROP TABLE IF EXISTS `stock`;
 CREATE TABLE  `stock` (
   `STOCK_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
